22e8100f0f00be59022f98893518c4fd55d54e45
vimal336 <asvimal336@gmail.com>
2024-05-16 10:29:13 +0530

    typescript practice

---
+++ main.ts
@@ -0,0 +1,9 @@
+function add(a, b ) {
+  var c = a;
+  return c;
+}
+
+var x = 10;
+x = 20;
+
+console.log(add(5, 10));
---
+++ main1.ts
@@ -0,0 +1,10 @@
+function sub(a, b ) {
+    var c = a-b;
+    return c;
+  }
+  
+  var x = 10;
+  x = 20;
+  
+  console.log(add(5, 10));
+  
