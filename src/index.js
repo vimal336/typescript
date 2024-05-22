8ce4ae34e9a13fbeb3e3fe251aff52e3a5a3f61a
vimal336 <asvimal336@gmail.com>
2024-05-22 11:18:09 +0530

    Typescript practice

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var x = 5;
+console.log(x);
+console.log(x + 4);
+var id = 5;
+var companyname = "vimal";
+// let anytype:any;
+// let anytype = "str"
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 let x = 5;
 console.log(x);
 console.log(x + 4);
-
-
 let id: number = 5;
+let companyname:string = "vimal";
+let iscompanyopen:boolean = true;
 
-
+//In TS we can't change type but using /any/ we can use dynamic type
+let anytype:any;
+anytype = "str"
+anytype = 7
+console.log(anytype);
